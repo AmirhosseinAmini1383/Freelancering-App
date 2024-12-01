21a80be54f9576c7df6b5e15ba75f10cb66812fd
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-12-01 19:55:03 +0330

    Send OTP form & Customize tailwind class

---
+++ src/features/authentication/SendOTPForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function SendOTPForm() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  return (
+    <div>
+      <form className="space-y-8">
+        <div>
+          <label className="mb-1" htmlFor="phoneNumber">
+            شماره موبایل
+          </label>
+          <input
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            id="phoneNumber"
+            className="textField__input"
+            type="text"
+          />
+        </div>
+        <button className="btn btn--primary w-full">ارسال کد تایید</button>
+      </form>
+    </div>
+  );
+}
+
+export default SendOTPForm;
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+import SendOTPForm from "../features/authentication/SendOTPForm";
+
+function Auth() {
+  return (
+    <div className="w-full mx-auto sm:max-w-sm">
+      <SendOTPForm />
+    </div>
+  );
+}
+
+export default Auth;
+
+// step
+// sendOTP
+// checkOTP
