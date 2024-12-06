deb30ca0339a17cef5b38c4a715a8ed055ec7230
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-12-06 19:07:54 +0330

    Send OTP handler with ReactQuery (useMutation)

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,12 @@
-import CheckOTPForm from "../features/authentication/CheckOTPForm";
 import SendOTPForm from "../features/authentication/SendOTPForm";
+// import CheckOTPForm from "../features/authentication/CheckOTPForm";
 
 function Auth() {
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
         <SendOTPForm />
-        <CheckOTPForm />
+        {/* <CheckOTPForm /> */}
       </div>
     </div>
   );
