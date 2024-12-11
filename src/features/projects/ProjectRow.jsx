f139452341c675052cae89eb81b8fbba8f2a167b
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-12-11 16:17:28 +0330

    Confirm Delete

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -6,9 +6,11 @@ import { HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
 import Modal from "../../ui/Modal";
 import { useState } from "react";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 function ProjectRow({ project, index }) {
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   return (
     <Table.Row>
       <td>{index + 1}</td>
@@ -35,19 +37,35 @@ function ProjectRow({ project, index }) {
       </td>
       <td>
         <div className="flex items-center gap-x-4">
-          <button onClick={() => setIsEditOpen(true)}>
-            <TbPencilMinus className="w-5 h-5 text-primary-900 " />
-          </button>
-          <Modal
-            open={isEditOpen}
-            title="modal title"
-            onClose={() => setIsEditOpen(false)}
-          >
-            This is modal
-          </Modal>
-          <button>
-            <HiOutlineTrash className="w-5 h-5 text-error " />
-          </button>
+          <>
+            <button onClick={() => setIsEditOpen(true)}>
+              <TbPencilMinus className="w-5 h-5 text-primary-900 " />
+            </button>
+            <Modal
+              open={isEditOpen}
+              title={`ویرایش ${project.title}`}
+              onClose={() => setIsEditOpen(false)}
+            >
+              This is modal
+            </Modal>
+          </>
+          <>
+            <button onClick={() => setIsDeleteOpen(true)}>
+              <HiOutlineTrash className="w-5 h-5 text-error " />
+            </button>
+            <Modal
+              open={isDeleteOpen}
+              title={`حذف ${project.title}`}
+              onClose={() => setIsDeleteOpen(false)}
+            >
+              <ConfirmDelete
+                resourceName={project.title}
+                onClose={() => setIsDeleteOpen(false)}
+                onConfirm={() => {}}
+                disabled={false}
+              />
+            </Modal>
+          </>
         </div>
       </td>
     </Table.Row>
