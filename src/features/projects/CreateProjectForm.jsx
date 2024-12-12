64dfa57f023a540c0af295aa6bcf4da53d4f8b22
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-12-12 18:10:40 +0330

    TextField with ReactHookForm (add React-Hook-Form in project)

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,17 +1,63 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
+import { useForm } from "react-hook-form";
 
 function CreateProjectForm() {
-  const [title, setTitle] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
-      <form className="">
+      <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
         <TextField
           label="عنوان پروژه"
           name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          register={register}
+          required
+          validationSchema={{
+            required: "عنوان پروژه را وارد کنید",
+            minLength: {
+              value: 5,
+              message: "طول عنوان پروژه باید حداقل ۵ کاراکتر باشد",
+            },
+          }}
+          errors={errors}
         />
+
+        <TextField
+          label="توضیحات پروژه"
+          name="description"
+          register={register}
+          required
+          validationSchema={{
+            required: "توضیحات پروژه را وارد کنید",
+            minLength: {
+              value: 15,
+              message: "طول توضیحات پروژه باید حداقل ۱۵ کاراکتر باشد",
+            },
+          }}
+          errors={errors}
+        />
+
+        <TextField
+          label="بودجه پروژه"
+          name="budget"
+          register={register}
+          type="number"
+          required
+          validationSchema={{
+            required: "بودجه پروژه را وارد کنید",
+          }}
+          errors={errors}
+        />
+
+        <button type="submit" className="btn btn--primary w-full">
+          تایید
+        </button>
       </form>
     </div>
   );
